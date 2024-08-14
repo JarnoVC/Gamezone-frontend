774fcf05e4729d41770cd2e3cca4288bc400609c
Jarno Van Clemen <jarno.vanclemen@hotmail.com>
2024-08-15 01:51:11 +0200

    Feat: added routes to new pages

--- src/main.js
+++ src/main.js
@@ -2,15 +2,22 @@ import './assets/main.css'
 
 import landingspage from './components/landingspagina.vue'
 import ProductUpload from './components/ProductUpload.vue'; 
+import ProductList from './components/ProductList.vue';
+import Signup from './components/Signup.vue';
+import Login from './components/Login.vue';
 
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue'
+import store from './store';
+
 
 const routes = [
     { path: '/', component: landingspage, name: 'home', meta: { requiresAuth: false } },
     { path: '/sell', component: ProductUpload, name: 'sell', meta: { requiresAuth: false } },
-    
+    { path: '/list', component: ProductList, name: 'list', meta: { requiresAuth: false } },
+    { path: '/signup', component: Signup, name: 'signup', meta: { requiresAuth: false } },
+    { path: '/login', component: Login, name: 'login', meta: { requiresAuth: false } },
 ]
 
 const router = createRouter({
@@ -27,4 +34,4 @@ const router = createRouter({
     },
 })
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
