29b1e2f8cd7740b958260e479b21a927629da12b
Jarno Van Clemen <jarno.vanclemen@hotmail.com>
2024-08-14 16:09:03 +0200

    Feat: added code for landingpage with styling

---
+++ src/components/landingspagina.vue
@@ -0,0 +1,78 @@
+<script setup>
+
+</script>
+
+<template>
+ 
+ <section class="home" id="home">
+
+    <div class="content">
+        <h3>The future of gaming</h3>
+        <p>Level Up Your Game. Discover the latest gear and exclusive deals, all in one place. Ready to conquer?</p>
+        <a href="#" class="btn">Getting Start</a>
+    </div>
+
+</section>
+
+</template>
+
+<style scoped>
+
+
+.btn{
+    margin-top: 1rem;
+    display: inline-block;
+    padding:.5rem 3rem;
+    font-size: 1.7rem;
+    color:#fff;
+    background: #904bff;
+    cursor: pointer;
+}
+.home{
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    background:url(../assets/home-img.jpeg) no-repeat;
+    background-size: cover;
+    background-position: center;
+    width: 100vw; 
+
+}
+
+.home .content{
+    max-width: 60rem;
+    padding-left: 6rem;
+}
+
+.home .content h3{
+    font-size: 6rem;
+    text-transform: uppercase;
+    color:#fff;
+}
+
+.home .content p{
+    font-size: 2rem;
+    font-weight: lighter;
+    line-height: 1.8;
+    padding:1rem 0;
+    color:#eee;
+}
+
+/* Responsive adjustments */
+@media (max-width: 768px) {
+  .home {
+    background-position: left;
+    justify-content: center;
+    text-align: center;
+  }
+
+  .home .content h3 {
+    font-size: 4.5rem;
+  }
+
+  .home .content p {
+    font-size: 1.5rem;
+  }
+}
+
+</style>
