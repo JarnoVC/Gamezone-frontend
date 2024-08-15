59ebcd4c065b85d46dab091f7f4c2d383137ccb9
Jarno Van Clemen <jarno.vanclemen@hotmail.com>
2024-08-15 18:56:27 +0200

    Feat: added api functions for orders

--- apiConnection.js
+++ apiConnection.js
@@ -25,13 +25,65 @@ export async function createProduct(ProductData) {
     }
 }
 
+export async function deleteProduct(ProductId) {
+  try {
+    const response = await apiClient2.delete(`/api/v1/products/${ProductId}`);
+    return response.data;
 
+  } catch (error) {
+    console.error('Error removing item from products:', error);
+    throw error;
+  }
+}
+
+export async function createOrder(productId, title, description) {
+  try {
+    const response = await apiClient2.post('/api/v1/orders', { productId, title, description });
+    return response.data;
+  } catch (error) {
+    console.error('Error creating order:', error.response?.data || error.message);
+    throw error;
+  }
+}
+
+export const updateOrderStatus = async (orderId, newStatus) => {
+  try {
+      // Ensure orderId is not undefined
+      if (!orderId) {
+          throw new Error('orderId is undefined');
+      }
+      const response = await apiClient2.put(`/api/v1/orders/${orderId}/status`, { status: newStatus });
+      return response.data;
+  } catch (error) {
+      console.error('Error updating order status:', error);
+      throw error;
+  }
+};
+
+export const fetchOrderCount = async () => {
+  try {
+      const response = await apiClient2.get(`/api/v1/orders/count`);
+      return response.data;
+  } catch (error) {
+      console.error('Error fetching order count:', error.response?.data || error.message);
+      throw error;
+  }
+};
 
 export default {
     fetchData() {
-      //return apiClient.get('/api/v1/two');
       return apiClient2.get('/api/v1/products');
-    }
+    },
+    fetchOrders() {
+      return apiClient2.get('/api/v1/orders');
+    },
+    updateOrderStatus(orderId, status) {
+      return apiClient2.put(`/api/v1/orders/${orderId}/status`, { status });
+    },
+    fetchOrderCount() {
+      return apiClient2.get('/api/v1/orders/count');
+    },
+    
    
 };
   
